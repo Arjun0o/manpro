592ff466bf8fff762f71dc7c961f3d4fa91c8bd7
Arjun Chauhan <arjun99.chauhan@gmail.com>
2019-09-17 16:22:57 +0530

    Worked on showing notifications and ordering projects in right order

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -9,7 +9,7 @@ import { Redirect } from 'react-router-dom';
 
 class Dashboard extends Component {
     render() {
-        const { projects, auth } = this.props;
+        const { projects, auth, notifications } = this.props;
         if (!auth.uid) return <Redirect to='/signin' />
         return (
             <div className={styles.container}>
@@ -17,7 +17,7 @@ class Dashboard extends Component {
                     <ProjectList projects={projects} />
                 </div>
                 <div>
-                    <Notifications />
+                    <Notifications notifications={notifications} />
                 </div>
             </div>
         )
@@ -26,12 +26,14 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
     return {
         projects: state.firestore.ordered.projects,
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        notifications: state.firestore.ordered.notifications
     }
 }
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects' }
+        { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
     ])
 )(Dashboard);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from './Notifications.module.css';
+import moment from 'moment';
 
 
-const Notifications = () => {
+const Notifications = (props) => {
+    const { notifications } = props;
     return (
         <div className={styles.container}>
             <h3 className={styles.text}>Notifications</h3>
             <ul className={styles.notification}>
-                <li>Notification</li>
-                <li>Notification</li>
-                <li>Notification</li>
+                {notifications && notifications.map(item => {
+                    return <li key={item.id}>
+                        <span className={styles.text1}>- {item.user} </span>
+                        <span className={styles.content}> {item.content}</span>
+                        <div className={styles.date}>{moment(item.time.toDate()).fromNow()}</div>
+                    </li>
+                })}
             </ul>
         </div>
     )
