818da2026153fad8ff29d0516718a8cf0e3e3f57
Arjun Chauhan <arjun99.chauhan@gmail.com>
2019-09-17 14:47:05 +0530

    Worked on the notifications component and it's styling

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -6,6 +6,11 @@ const Notifications = () => {
     return (
         <div className={styles.container}>
             <h3 className={styles.text}>Notifications</h3>
+            <ul className={styles.notification}>
+                <li>Notification</li>
+                <li>Notification</li>
+                <li>Notification</li>
+            </ul>
         </div>
     )
 }
